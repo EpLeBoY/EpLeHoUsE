0fcec509170c3469fd1cb675573581da3162599a
EpLeBoY <115426326+EpLeBoY@users.noreply.github.com>
2022-12-04 18:10:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var github = function(){
+    window.location = "https://github.com/EpLeBoY";
+}
+var discord = function(){
+    window.location = "https://discord.gg/tb7XMRnz";
+}
+
+var reddit = function(){
+    window.location = "https://www.reddit.com/user/EpLe_BoY"
+}
